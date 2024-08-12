84a89a936f5dcac0eeec77ea280e681b390a1e89
Duberly, Guarnizo Fuentes Rivera <duberlygfr@gmail.com>
2024-08-12 14:52:08 -0500

    Initial commit

---
+++ src/main/java/com/nttdata/archunitdemo1/Archunitdemo1Application.java
@@ -0,0 +1,13 @@
+package com.nttdata.archunitdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Archunitdemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Archunitdemo1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdata/archunitdemo1/Archunitdemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.archunitdemo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Archunitdemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
